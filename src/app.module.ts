04faf11848bd51715128b5bdc3e1a653a0ec8223
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 01:26:51 +0600

    clean up app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { AppService } from './app.service';
 import typeorm from './typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Player } from './entities/Player';
+import { Reward } from './entities/Reward';
+import { CouponController } from './coupon/coupon.controller';
+import { CouponService } from './coupon/coupon.service';
+import { Coupon } from './entities/Coupon';
+import { PlayerCoupon } from './entities/PlayerCoupon';
 
 @Module({
   imports: [
@@ -16,8 +22,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: async (configService: ConfigService) =>
         configService.get('typeorm'),
     }),
+    TypeOrmModule.forFeature([Player, Reward, Coupon, PlayerCoupon]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CouponController],
+  providers: [AppService, CouponService],
 })
 export class AppModule {}
