001cea0f49435ef82c44e711fb8cc63e75b2f710
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 02:16:49 +0600

    find coupon with related reward from db by coupon id

--- src/coupon/coupon.service.ts
+++ src/coupon/coupon.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coupon } from 'src/entities/Coupon';
+import { Player } from 'src/entities/Player';
+import { PlayerCoupon } from 'src/entities/PlayerCoupon';
+import { Reward } from 'src/entities/Reward';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CouponService {}
+export class CouponService {
+  constructor(
+    @InjectRepository(Player) private readonly player: Repository<Player>,
+    @InjectRepository(Coupon) private readonly coupon: Repository<Coupon>,
+    @InjectRepository(Reward) private readonly reward: Repository<Reward>,
+    @InjectRepository(PlayerCoupon)
+    private readonly playerCoupon: Repository<PlayerCoupon>,
+  ) {}
+
+  findByIdWithReward(id: number): Promise<Coupon | null> {
+    return this.coupon.findOne({
+      where: { id },
+      relations: {
+        Reward: true,
+      },
+    });
+  }
+}
