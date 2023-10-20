6d40fd7f88742c841b9de419368ca4c8c45e7cb3
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 02:21:24 +0600

    find player by id

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Player } from 'src/entities/Player';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PlayerService {}
+export class PlayerService {
+  constructor(
+    @InjectRepository(Player) private readonly player: Repository<Player>,
+  ) {}
+
+  findById(id: number): Promise<Player | null> {
+    return this.player.findOneBy({ id });
+  }
+}
