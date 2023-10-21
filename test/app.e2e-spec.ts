88e22c1bb84b9b67729475bed5c2bb4c54164f04
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 11:46:17 +0600

    drop data from player coupon after e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { connectionSource } from '../src/typeorm';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let connection: DataSource;
 
   beforeEach(async () => {
+    connection = await connectionSource.initialize();
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -23,6 +28,15 @@ describe('AppController (e2e)', () => {
   });
 
   afterAll(async () => {
+    const entities = connectionSource.entityMetadatas;
+
+    for (let entity of entities) {
+      if (entity.name === 'PlayerCoupon') {
+        const repo = connection.getRepository(entity.name);
+        await repo.clear();
+      }
+    }
+    await connection.destroy();
     await app.close();
   });
 });
