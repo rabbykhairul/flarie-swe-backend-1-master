d9e869d9df869adb3c3c9099e5937c54e12747e1
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 11:54:08 +0600

    create e2e test form coupon controller

---
+++ test/coupon.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { connectionSource } from '../src/typeorm';
+import { DataSource } from 'typeorm';
+
+describe('CouponController (e2e)', () => {
+  let app: INestApplication;
+  let connection: DataSource;
+
+  beforeAll(async function setUpDbConnection() {
+    connection = await connectionSource.initialize();
+  });
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+
+  afterAll(async () => {
+    const entities = connectionSource.entityMetadatas;
+
+    for (let entity of entities) {
+      if (entity.name === 'PlayerCoupon') {
+        const repo = connection.getRepository(entity.name);
+        await repo.clear();
+      }
+    }
+    await connection.destroy();
+    await app.close();
+  });
+});
