a9121936dac41b0fb6554a30eabe6ddd381ac8b6
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 15:59:26 +0600

    seed migration to generate reward campaigns in db

---
+++ migrations/1697881836519-seed-rewards.ts
@@ -0,0 +1,34 @@
+import { addDays, subDays } from 'date-fns';
+import { connectionSource } from '../src/typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const startDate = new Date();
+const endDate = addDays(startDate, 7);
+
+const rewardCampigns = [
+  {
+    name: 'DiGiCell Eid Rewards',
+    startDate,
+    endDate,
+    perDayLimit: 3,
+    totalLimit: 21,
+  },
+  {
+    name: 'Summer dicsounts (expired)',
+    startDate: subDays(startDate, 8),
+    endDate: subDays(startDate, 1),
+    perDayLimit: 3,
+    totalLimit: 21,
+  },
+];
+
+export class SeedRewards1697881836519 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const repository = connectionSource.getRepository('Reward');
+    for (let reward of rewardCampigns) {
+      await repository.save(reward);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
