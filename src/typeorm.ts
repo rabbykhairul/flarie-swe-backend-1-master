18f4c69c3c6aff91ede2107ec50bf5b308a4402d
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 14:08:09 +0600

    run e2e tests against testing database

--- src/typeorm.ts
+++ src/typeorm.ts
@@ -8,17 +8,22 @@ import { PlayerCoupon } from './entities/PlayerCoupon';
 
 dotenvConfig({ path: '.env' });
 
+
 const config = {
   type: 'mysql',
   host: `${process.env.TYPEORM_HOST}`,
-  port: `${process.env.TYPEORM_PORT}`,
+  port:
+    process.env.NODE_ENV === 'test' ? '3700' : `${process.env.TYPEORM_PORT}`,
   username: `${process.env.TYPEORM_USERNAME}`,
   password: `${process.env.TYPEORM_PASSWORD}`,
-  database: `${process.env.TYPEORM_DATABASE}`,
+  database:
+    process.env.NODE_ENV === 'test'
+      ? 'test_db'
+      : `${process.env.TYPEORM_DATABASE}`,
   entities: [Player, Reward, Coupon, PlayerCoupon],
   migrations: process.env.typeorm === 'true' ? ['migrations/*.ts'] : [],
   autoLoadEntities: true,
-  synchronize: false,
+  synchronize: process.env.NODE_ENV === 'test',
 };
 
 export default registerAs('typeorm', () => config);
